b6cf796f644a8a3dd1587bc9d3d1ff04b7558c48
katekr <fatooma.sj@gmail.com>
2026-05-28 19:54:49 -0400

    Initial Commit

---
+++ AidaMobile/AidaMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AidaMobileApp.swift
+//  AidaMobile
+//
+//  Created by Fate Sj on 2026-05-28.
+//
+
+import SwiftUI
+
+@main
+struct AidaMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AidaMobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AidaMobile
+//
+//  Created by Fate Sj on 2026-05-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
